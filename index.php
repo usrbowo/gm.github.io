a174daaca03a296afb5e834e0cfdbb4690fbb610
usrbowo <129925038+usrbowo@users.noreply.github.com>
2024-10-26 22:28:25 +0700

    Update index.html

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<?php include 'nav.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sebutkan Nama Dari Gambar Itu</title>
+    <!-- <script type="text/tailwindcss" src="https://cdn.tailwindcss.com"></script> -->
+    
+    <link rel="stylesheet" href="./src/style.css">
+
+</head>
+ <body>
+    <!-- navbar star -->
+    <nav class="navbar">
+        <a href="#" class="navbar-logo">GA<span>ME</span></a>
+        <div class="navbar-nav">
+          <!-- <a href="#about"><img class="img-game" src="start/games.png" alt=""></a>
+          <a href="#about"><img class="img-game" src="start/games.png" alt=""></a> -->
+          <a href="">LV.1</a>
+          <a href="">Tebak Gambar V.1</a>
+          <a href="">Update soon</a>
+        </div>
+       
+      </nav>
+      <!-- end navbar -->
+     
 <div class="container">
     <h2 class="title" id="message">TEBAK TULISAN</h2>
     <div class="screen">
